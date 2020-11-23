20191cdcae60da4e9bfc76f2ec1471ca1549caa2
Derek Stephen McLean <delboy1978uk@gmail.com>
2020-11-23 13:00:10 +0100

    MiddlewareRegistrationInterface

--- src/MiddlewareAwareInterface.php
+++ src/MiddlewareAwareInterface.php
@@ -5,10 +5,14 @@ namespace Bone\Http;
 use Barnacle\Container;
 use Bone\Http\Middleware\Stack;
 
-interface MiddlewareAwareInterface
+interface MiddlewareAwareInterface extends MiddlewareRegistrationInterface
 {
     /**
+     * @deprecated please use MiddlewareRegistrationInterface
+     *             and just return an array of your Middleware
+     *             instead of adding to the container
      * @param Stack $stack
+     * @param Container $container
      */
     public function addMiddleware(Stack $stack, Container $container): void;
 }
