5a947a386ecbc1c63646ab7805a9d3633c2e2b53
Derek Stephen McLean <delboy1978uk@gmail.com>
2020-11-27 04:17:30 +0100

    adding attributes to responses

--- src/Response.php
+++ src/Response.php
@@ -2,9 +2,10 @@
 
 namespace Bone\Http;
 
+use Bone\Server\Traits\HasAttributesTrait;
 use Laminas\Diactoros\Response as BaseResponse;
 
 class Response extends BaseResponse
 {
-
+    use HasAttributesTrait;
 }
--- src/Response/HtmlResponse.php
+++ src/Response/HtmlResponse.php
@@ -2,9 +2,57 @@
 
 namespace Bone\Http\Response;
 
-use Laminas\Diactoros\Response\HtmlResponse as BaseResponse;
+use Bone\Http\Response;
+use Bone\Server\Traits\HasAttributesTrait;
+use Laminas\Diactoros\Response\InjectContentTypeTrait;
 
-class HtmlResponse extends BaseResponse
+class HtmlResponse extends Response
 {
+    use InjectContentTypeTrait;
 
+    /**
+     * Create an HTML response.
+     *
+     * Produces an HTML response with a Content-Type of text/html and a default
+     * status of 200.
+     *
+     * @param string|StreamInterface $html HTML or stream for the message body.
+     * @param int $status Integer status code for the response; 200 by default.
+     * @param array $headers Array of headers to use at initialization.
+     * @throws Exception\InvalidArgumentException if $html is neither a string or stream.
+     */
+    public function __construct($html, int $status = 200, array $headers = [])
+    {
+        parent::__construct(
+            $this->createBody($html),
+            $status,
+            $this->injectContentType('text/html; charset=utf-8', $headers)
+        );
+    }
+
+    /**
+     * Create the message body.
+     *
+     * @param string|StreamInterface $html
+     * @throws Exception\InvalidArgumentException if $html is neither a string or stream.
+     */
+    private function createBody($html) : StreamInterface
+    {
+        if ($html instanceof StreamInterface) {
+            return $html;
+        }
+
+        if (! is_string($html)) {
+            throw new Exception\InvalidArgumentException(sprintf(
+                'Invalid content (%s) provided to %s',
+                (is_object($html) ? get_class($html) : gettype($html)),
+                __CLASS__
+            ));
+        }
+
+        $body = new Stream('php://temp', 'wb+');
+        $body->write($html);
+        $body->rewind();
+        return $body;
+    }
 }
