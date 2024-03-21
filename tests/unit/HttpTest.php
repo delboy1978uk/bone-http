f7e16237f494e1f7ee55f52eb8838e7c87f29feb
Derek Stephen McLean <delboy1978uk@gmail.com>
2024-03-21 01:27:31 +0100

    add use statement

--- tests/unit/HttpTest.php
+++ tests/unit/HttpTest.php
@@ -11,6 +11,7 @@ use Bone\Http\Middleware\JsonParse;
 use Bone\Http\Middleware\Stack;
 use Bone\Http\Response\HtmlResponse;
 use Bone\Http\Response\LayoutResponse;
+use Bone\Http\RouterInterface;
 use League\Route\Router;
 use BoneTest\Http\AnotherFakeRequestHandler;
 use BoneTest\Http\FakeController;
