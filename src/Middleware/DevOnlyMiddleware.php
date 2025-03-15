b71cb0cc88c985e2f9da63a898ba361f50dfe32f
Derek Stephen McLean <delboy1978uk@gmail.com>
2025-03-15 13:51:28 +0100

    dev only middleware

---
+++ src/Middleware/DevOnlyMiddleware.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Bone\Http\Middleware;
+
+use Bone\Exception;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\MiddlewareInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+use function getenv;
+
+class DevOnlyMiddleware implements MiddlewareInterface
+{
+    public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
+    {
+        if (getenv('APPLICATION_ENV' === 'development')) {
+            return $handler->handle($request);
+        }
+
+        throw new Exception(Exception::LOST_AT_SEA) ;
+    }
+}
